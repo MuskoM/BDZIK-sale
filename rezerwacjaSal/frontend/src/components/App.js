a814dc479f6a382788a770bddf9c4850dbfd3cca
Adam Lisowski <adam.foxman@outlook.com>
2020-11-19 15:06:56 +0100

    React config

--- rezerwacjaSal/frontend/src/components/App.js
+++ rezerwacjaSal/frontend/src/components/App.js
@@ -4,9 +4,7 @@ import ReactDOM from 'react-dom';
 class App extends Component{
     render() {
         return (
-            <div>
-                <h1>React APP</h1>
-            </div>
+
         );
     }
 }
