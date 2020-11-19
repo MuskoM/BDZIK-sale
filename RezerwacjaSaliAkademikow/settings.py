60e4d3d5082ca5ce8ad921c041fa70eb4e0f7119
Mateusz Musko <wi105633@student.pb.edu.pl>
2020-11-19 16:49:18 +0100

    PTRSBDZIK-40 #time 5m Rebased workspace tree

--- RezerwacjaSaliAkademikow/settings.py
+++ RezerwacjaSaliAkademikow/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'RezerwacjaSaliAkademikow.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR+'/templates']
+        'DIRS': [BASE_DIR + '/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
