44b62627fba1aee41e64111a411e6c3a1616e5e4
Mateusz Musko <wi105633@student.pb.edu.pl>
2020-11-23 19:05:48 +0100

    Added user and logout api/auth/

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path,include
-from .views import RegisterAPI,LoginAPI
+from .views import RegisterAPI,LoginAPI,UserAPI
 from knox import views as knox_views
 
 urlpatterns = [
     path('api/auth',include('knox.urls')),
     path('api/auth/register',RegisterAPI.as_view()),
-    path('api/auth/login',LoginAPI.as_view())
+    path('api/auth/login',LoginAPI.as_view()),
+    path('api/auth/user',UserAPI.as_view()),
+    path('api/auth/logout', knox_views.LogoutView.as_view(), name="knx_logout")
 ]
