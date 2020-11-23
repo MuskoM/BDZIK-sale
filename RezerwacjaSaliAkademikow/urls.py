4a98f6571a17bcec4d21ac7badecdfcede086d54
Mateusz Musko <wi105633@student.pb.edu.pl>
2020-11-23 14:11:17 +0100

    Created Login and Register API's

--- RezerwacjaSaliAkademikow/urls.py
+++ RezerwacjaSaliAkademikow/urls.py
@@ -5,5 +5,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reservations/', include("frontend.urls")),
-    path('reservations/', include('reservations.urls'))
+    path('reservations/', include('reservations.urls')),
+    path('reservations/', include('accounts.urls'))
 ]
