68af92eb98c6c469a0bcb86c80ab74b45b0ed7ed
Adam Lisowski <adam.foxman@outlook.com>
2021-01-01 23:29:19 +0100

    Main page created, made attempt in creating FacultiesTemplate.html

--- reservations/views.py
+++ reservations/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
 from django.views import View
 
+from reservations.models import Wydzial, Akademik, Pomieszczenie
+
 
 class MainSite(View):
     def get(self, request):
-        return render(request, 'reservations/MainTemplate.html')
+        faculties = Wydzial.objects.all()
+        dorms = Akademik.objects.all()
+
+        context = {
+            "faculties": faculties,
+            "dormitories": dorms
+        }
+
+        return render(request, 'reservations/MainTemplate.html', context)
 
 
 class FacultiesView(View):
     def get(self, request, faculty_id):
-        return render(request, 'reservations/FacultiesTemplate.html')
+        faculty_id = faculty_id
+        faculty = Wydzial.objects.get(id_wydzialu=faculty_id)
+        classrooms = Pomieszczenie.objects.filter(id_wydzialu=faculty_id)
+
+        context = {
+            "faculty_name": faculty,
+            "faculty_id": faculty_id,
+            "classrooms": classrooms
+        }
+        return render(request, 'reservations/FacultiesTemplate.html', context)
 
 
 class FacultyRoomView(View):
@@ -24,20 +43,20 @@ class DormView(View):
 
 class DormRoomView(View):
     def get(self, request, dorm_id, room_id):
-        return render(request,"reservations/DormRoomTemplate.html")
+        return render(request, "reservations/DormRoomTemplate.html")
 
 
 class UserView(View):
-    def get(self,request):
+    def get(self, request):
         return render(request, 'reservations/UserTemplate.html')
 
 
 class UserEditView(View):
-    def get(self,request):
+    def get(self, request):
         return render(request, 'reservations/UserEditTemplate.html')
 
 
 class UserReservationsView(View):
-    def get(self,request, reservation_status):
-        return render(request,'reservations/UserReservationsTemplate.html',
+    def get(self, request, reservation_status):
+        return render(request, 'reservations/UserReservationsTemplate.html',
                       context={"status": reservation_status})
